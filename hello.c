1beaa3d740ebb07ea5a2e75cbc3eb5ee92eff8bc
krishnendu-Vision <ckrishnendu76@gmail.com>
2026-05-17 11:01:02 +0530

    Updating files!

--- hello.c
+++ hello.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
 //it's a simple hello world program
-//Testing git automation
-//Again Testing git automation
-//Final git automation Testing
-//Checking files auto update
 int main() {
     printf("Hello world!\n");
 
