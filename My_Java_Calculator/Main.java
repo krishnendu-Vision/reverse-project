9a02ad8b979e1666c3d5ffb0186333cca2f2472b
krishnendu-Vision <ckrishnendu76@gmail.com>
2026-05-27 22:59:58 +0530

    Update, code formatting!

--- My_Java_Calculator/Main.java
+++ My_Java_Calculator/Main.java
@@ -22,8 +22,8 @@ public class Main {
 
            int statusCode = switch (option) {
 
-                case "1" -> {                  System.out.println("---This is addition section---");
-
+                case "1" -> {
+                    System.out.println("---This is addition section---");
 
                     System.out.print("Enter first number: ");
                     double a = Double.parseDouble(sc.nextLine());
